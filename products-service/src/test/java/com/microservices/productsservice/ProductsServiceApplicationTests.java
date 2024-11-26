eef1e3c2c2d8e3b23df59655e452cd4502ae1301
DonovanJC <donovanjc@hotmail.es>
2024-11-26 22:36:04 +0000

    Products Service - Tests created

--- products-service/src/test/java/com/microservices/productsservice/ProductsServiceApplicationTests.java
+++ products-service/src/test/java/com/microservices/productsservice/ProductsServiceApplicationTests.java
@@ -1,15 +1,65 @@
 package com.microservices.productsservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductsServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
     }
 
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name":"2: test1",
+                    "description":"2: testing the test testera",
+                    "price":1.2
+                }
+                """;
+
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("2: test1"));
+    }
+
+    @Test
+    void shouldGetAllProducts() {
+        // Retrieve all products and verify that the product we just created is part of the list
+        RestAssured.given()
+                .when()
+                .get("/api/product")  // GET request to fetch all products
+                .then()
+                .statusCode(200)
+                .body("name", Matchers.hasItem("2: test1")); // Verify the product name is in the lis
+    }
 }
